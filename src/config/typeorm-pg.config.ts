2aa476277a625f91c3eeb2d89cca72d623bce565
kartemdev <ihctuyd@mail.ru>
2023-09-06 10:47:29 +0300

    feat: ML-22 create config for deploy on vercel

--- src/config/typeorm-pg.config.ts
+++ src/config/typeorm-pg.config.ts
@@ -7,9 +7,10 @@ require('dotenv').config();
 
 export const databaseConfig: DataSourceOptions = {
   type: 'postgres',
+  url: process.env.POSTGRES_URL,
   host: process.env.POSTGRES_HOST,
   port: +process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USERNAME,
+  username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   entities: [path.join(__dirname, '/../**/*.entity{.js, .ts}')],
