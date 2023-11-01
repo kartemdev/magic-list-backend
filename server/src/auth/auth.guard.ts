38fc1fdd63ac70989667f08487ebeb96be54b28e
kartemdev <ihctuyd@mail.ru>
2023-11-01 13:32:39 +0300

    feat: ML-42 create auth guard

---
+++ server/src/auth/auth.guard.ts
@@ -0,0 +1,55 @@
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SessionService } from 'src/session/session.service';
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private sessionService: SessionService) {}
+
+  private parseCookies(cookieString: string) {
+    if (!cookieString) {
+      return null;
+    }
+
+    return cookieString.split(';').reduce((acc, cookie) => {
+      const cookieName = cookie.split('=')[0].trim();
+      const cookieValue = cookie.split('=')[1].trim();
+
+      return { ...acc, [cookieName]: cookieValue };
+    }, {} as Record<string, string>);
+  }
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    try {
+      const { authorization, cookie, ['user-agent']: headerUserAgent } = request.headers;
+
+      const bearer = authorization.split(' ')[0];
+      const token = authorization.split(' ')[1];
+
+      if (bearer !== 'Bearer' || !token) {
+        throw new HttpException('unauthorized', HttpStatus.UNAUTHORIZED) 
+      }
+
+      const cookieRefreshId = this.parseCookies(cookie)?.['ml_uuid'];
+
+      const { id, refreshId, expiresIn, userAgent } = await this.sessionService.get(cookieRefreshId);
+
+      if (
+        !this.jwtService.verify(token, { secret: process.env.ACCESS_SECRET_KEY }) ||
+        cookieRefreshId !== refreshId ||
+        headerUserAgent !== userAgent ||
+        new Date().getTime() >= expiresIn
+      ) {
+        await this.sessionService.delete(id);
+        throw new HttpException('unauthorized', HttpStatus.UNAUTHORIZED);
+      }
+
+      return true;
+    } catch {
+      throw new HttpException('unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   providers: [UserService],
   exports: [UserService],
 })
